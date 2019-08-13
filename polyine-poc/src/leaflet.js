9768dd4e7281dd891a8a0cfe5f8eac65b13b6b78
Sukumar, Srigar - Srigar <vc.srigar.sukumar@lowes.com>
2019-08-13 16:30:39 +0530

    Distance calculation

--- polyine-poc/src/leaflet.js
+++ polyine-poc/src/leaflet.js
@@ -4,7 +4,25 @@ import { Map, TileLayer, Polyline } from "react-leaflet";
 // import { latLongGeo } from './tempLatLong';
 import { latLongGeo } from './convertcsv';
 
+// Returning distance in km
+const getDistance = (origin, destination) => {
+  var lon1 = toRadian(origin[1]),
+      lat1 = toRadian(origin[0]),
+      lon2 = toRadian(destination[1]),
+      lat2 = toRadian(destination[0]);
 
+  var deltaLat = lat2 - lat1;
+  var deltaLon = lon2 - lon1;
+
+  var a = Math.pow(Math.sin(deltaLat/2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(deltaLon/2), 2);
+  var c = 2 * Math.asin(Math.sqrt(a));
+  var EARTH_RADIUS = 6371;
+  return (c * EARTH_RADIUS).toFixed(2);
+}
+
+const toRadian = (degree) => {
+  return degree*Math.PI/180;
+};
 
 export default class Leaflet extends React.Component {
   constructor(props) {
@@ -13,7 +31,7 @@ export default class Leaflet extends React.Component {
       
       lat: 32.34756253,
       lng: 34.8613223,
-      zoom: 11,
+      zoom: 14,
       latLongGeo:latLongGeo,
       data: [
         {
@@ -61,6 +79,9 @@ export default class Leaflet extends React.Component {
           />
 
             {this.state.latLongGeo.features.map((feature) => {
+              // Stored distance in same object.
+              const { lat0, lng0, lat1, lng1 } = feature.properties;
+              feature.distance = getDistance([lat0, lng0], [lat1, lng1]);
             //   console.log('************************')
             // console.log([feature.properties.lat0, feature.properties.lng0], [feature.properties.lat1, feature.properties.lng1])
             // console.log('########################')
